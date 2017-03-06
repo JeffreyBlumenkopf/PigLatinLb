57a4d37e05a6020b1cfc2ffe9e8bb6828e408424
Jeffrey Blumenkopf <jeffrey.blumenkopf@gmail.com>
2017-03-06 09:25:34 -0500

    added continue loop

--- src/com/jetbrains/Main.java
+++ src/com/jetbrains/Main.java
@@ -5,41 +5,45 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        String userContinue = "n";
+        do {
+            String subString = "";
+            Scanner scan1 = new Scanner(System.in);
 
-        String subString = "";
-        Scanner scan1 = new Scanner(System.in);
-
-        System.out.println("Enter a word to be translated:");
-        String userInput = scan1.nextLine();
-
-
-        if ((userInput.charAt(0) == 'a') || (userInput.charAt(0) == 'e') || (userInput.charAt(0) == 'i')
-                || (userInput.charAt(0) == 'o') || (userInput.charAt(0) == 'u')) {
+            System.out.println("Enter a word to be translated:");
+            String userInput = scan1.nextLine();
 
 
-            userInput = userInput + "way";
+            if ((userInput.charAt(0) == 'a') || (userInput.charAt(0) == 'e') || (userInput.charAt(0) == 'i')
+                    || (userInput.charAt(0) == 'o') || (userInput.charAt(0) == 'u')) {
 
-            System.out.println(userInput);
+
+                userInput = userInput + "way";
+
+                System.out.println(userInput);
 
 
-        } else {
+            } else {
 
-            int vowelIndex = 0;
+                int vowelIndex = 0;
 
-            for (vowelIndex = 0; vowelIndex < userInput.length(); vowelIndex++) {
+                for (vowelIndex = 0; vowelIndex < userInput.length(); vowelIndex++) {
 
-                char charOne = userInput.charAt(vowelIndex);
+                    char charOne = userInput.charAt(vowelIndex);
 
-                if (charOne == 'a' || charOne == 'e' || charOne == 'i' || charOne == 'o' || charOne == 'u') {
-                    subString = userInput.substring(vowelIndex); /// return substring from a e i ou till end of word
-                    subString = subString + userInput.substring(0, (vowelIndex)) + "ay";  //   add charAt0,  to end of word then add "ay"
-                    System.out.println(subString);
-                    break;
+                    if (charOne == 'a' || charOne == 'e' || charOne == 'i' || charOne == 'o' || charOne == 'u') {
+                        subString = userInput.substring(vowelIndex); /// return substring from a e i ou till end of word
+                        subString = subString + userInput.substring(0, (vowelIndex)) + "ay";  //   add charAt0,  to end of word then add "ay"
+                        System.out.println(subString);
+                        break;
+                    }
+
                 }
 
             }
-
-        }
+            System.out.println("Do you want to enter another word? (y/n)");
+            userContinue = scan1.nextLine();
+        } while (userContinue.equalsIgnoreCase("y"));
 
 
     }
